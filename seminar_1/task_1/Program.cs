0bc958c715f9ab27ccec953483ba4004547896ab
Aleksey <alekseyzhilin@mail.ru>
2023-06-11 16:46:24 +0300

    семинар 1

---
+++ seminar_1/task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число:");
+string? numberString = Console.ReadLine()!;
+int number = Convert.ToInt32(numberString);
+Console.WriteLine(number * number);
+if (number == 100){
+    Console.WriteLine("число 100");
+}
+else{
+    Console.WriteLine("число отличное от 100");
+}
+
+for(int i = -number; i <= number; i++){
+    Console.WriteLine(i);
+}
