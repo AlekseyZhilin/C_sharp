0e81c0d3fade5455bfc320fcd9e225c450b2ab42
Aleksey <alekseyzhilin@mail.ru>
2023-06-28 20:25:25 +0300

    add branch

---
+++ 2nArray/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+int[,] array = new int[2, 4];
+
+void PrintArray(int[,] array){
+    for(int i = 0; i < array.GetLength(0); i ++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(array);
