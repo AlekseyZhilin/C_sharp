d67b9f8c2cfcafa8c0e5a11816f5319e76519c5b
Aleksey <alekseyzhilin@mail.ru>
2023-06-28 22:51:17 +0300

    home work_6

--- 2nArray/Program.cs
+++ 2nArray/Program.cs
@@ -13,3 +13,30 @@ void PrintArray(int[,] array){
 }
 
 PrintArray(array);
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Difference(int number){
+    int delit = number / 2;
+    Console.WriteLine(number - delit * 2);
+    if(delit != 0) return Difference(delit);
+    return -1;
+}
+
+Difference(number);
+Console.WriteLine();
+
+double resLog = Math.Log2(number);
+int digits = Convert.ToInt32(resLog);
+// if(resLog != Convert.ToInt32(resLog)) digits ++;
+digits++;
+
+int curPos = 0;
+int [] binVal = new int[digits];
+while(number != 0){
+    binVal[digits - curPos - 1] = number % 2;
+    number /= 2;
+    curPos++;
+}
+Console.WriteLine($"[ {string.Join(" ", binVal)} ]");
